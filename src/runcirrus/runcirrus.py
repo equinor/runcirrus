8ac626f88c4be3175c11bc0dd763d61498e27d5f
Zohar Malamant <ZOM@equinor.com>
2025-04-29 17:07:27 +0200

    Add stub logger

--- src/runcirrus/runcirrus.py
+++ src/runcirrus/runcirrus.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3.11
 """Wrapper for running Cirrus with MPI in Equinor.
 
+
 This script operates on Cirrus .in files and enables you to simulate in
 parallell. For example, given the "spe1.in" case, you can simply run it with the
 following command:
@@ -45,61 +46,7 @@ import re
 import socket
 from pathlib import Path
 from dataclasses import dataclass
-from time import time
-
-
-EVENT_NAMESPACE = ""
-EVENT_HUB = ""
-SAS_KEY_NAME = ""
-SAS_KEY_VALUE = b""
-
-
-def az_create_sas_token(uri: str) -> str:
-    """Create SAS token to be able to interact with the Event Hub"""
-    import hmac
-    import urllib.parse
-    import base64
-
-    ttl = time() + 86400
-
-    quri = urllib.parse.quote_plus(uri)
-
-    sign_key = f"{quri}\n{int(ttl)}".encode("utf-8")
-    signature = base64.b64encode(
-        hmac.new(SAS_KEY_VALUE, sign_key, "sha256").digest()
-    ).decode("utf-8")
-    qsig = urllib.parse.quote(signature)
-    token = (
-        f"SharedAccessSignature sr={quri}&sig={qsig}&se={int(ttl)}&skn={SAS_KEY_NAME}"
-    )
-    return token
-
-
-def az_send_event(data: dict[str, Any]) -> None:
-    """Send data to Microsoft Azure Event Hub"""
-    if not all([EVENT_NAMESPACE, EVENT_HUB, SAS_KEY_NAME, SAS_KEY_VALUE]):
-        return
-
-    import http.client
-    import json
-
-    uri = f"https://{EVENT_NAMESPACE}.servicebus.windows.net/{EVENT_HUB}/messages"
-    body = json.dumps(data)
-    headers = {
-        "Authorization": az_create_sas_token(uri),
-        "Content-Length": str(len(body)),
-        "Content-Type": "application/json",
-    }
-
-    conn = http.client.HTTPSConnection(f"{EVENT_NAMESPACE}.servicebus.windows.net")
-    conn.request("POST", f"/{EVENT_HUB}/messages", body=body, headers=headers)
-
-    if (resp := conn.getresponse()) and resp.status != 201:
-        print("Failure during logging:", file=sys.stderr)
-        print(f"HTTP Status: {resp.status} ({resp.reason})", file=sys.stderr)
-        print(resp.read(), file=sys.stderr)
-
-    conn.close()
+from runcirrus.logger import logger
 
 
 def anonymize_fqdn(fqdn: str) -> str:
@@ -433,11 +380,12 @@ def main() -> None:
         telemetry=args.telemetry,
     )
 
-    az_send_event(
-        {
+    logger.info(
+        "Start job",
+        extra={
             "type": "runcirrus",
             "script": sys.argv[0],
-            "version": args.version,
+            "version": str(args.version),
             "num_tasks_per_machine": args.num_tasks_per_machine,
             "num_machines": args.num_machines,
             "num_tasks": num_tasks,
@@ -445,7 +393,7 @@ def main() -> None:
             "bsub": HAVE_BSUB,
             "qsub": HAVE_QSUB,
             "hostname": anonymize_fqdn(socket.getfqdn()),
-        }
+        },
     )
 
     if args.print_job_script:
